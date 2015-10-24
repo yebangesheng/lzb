d0a87c80cbb8b35ac672e92f212e717b336de6f4
jl <lizhongbo@lizhongbodeMac-Pro.local>
2015-10-24 15:04:06 -0700

    初始化我的项目

---
+++ Mypro/Mypro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mypro
+//
+//  Created by lizhongbo on 15/10/24.
+//  Copyright © 2015年 lizhongbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mypro/Mypro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mypro
+//
+//  Created by lizhongbo on 15/10/24.
+//  Copyright © 2015年 lizhongbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
